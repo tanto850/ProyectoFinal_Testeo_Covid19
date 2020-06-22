a54a586f1537c1d74d7829152158f35f3c1bcaf8
HUMANA  GUSTAVO <HUMANA  GUSTAVO@LAPTOP-2M34BLRB>
2020-06-22 15:49:33 -0300

    asunto:renombramiento de paquetes, midificacion de index. responsable: humana hernan task03: renonbramiento del paquete controller para evitar inconvenientes, incrporacion de pagina inicio de secion, modificacion de formulario que registra usuarios.

---
+++ ProyectoFinal_Testeo_Covid19/src/main/java/ar/edu/unju/fi/ProyectoFinalTesteoCovid19Aplication.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoFinalTesteoCovid19Aplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoFinalTesteoCovid19Aplication.class, args);
+	}
+
+}
---
+++ ProyectoFinal_Testeo_Covid19/src/main/java/ar/edu/unju/fi/controller/MainController.java
@@ -0,0 +1,26 @@
+package ar.edu.unju.fi.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+	
+	@RequestMapping("/index")
+	public String getIndex(Model model) {
+		return "inicio";
+	}
+	
+	@RequestMapping("inicio")
+	public String getInicio(Model model) {
+		return "inicio";
+	}
+	
+	@RequestMapping("cuenta")
+	public String getCuenta(Model model) {
+		return "cuenta";
+	}
+	
+
+}
