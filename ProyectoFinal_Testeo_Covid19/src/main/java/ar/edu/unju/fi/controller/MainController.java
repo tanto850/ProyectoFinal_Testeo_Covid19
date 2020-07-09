9e328c1d828d3fc05f1804fe7f83c487b645dc5f
user <user@Analia-Aucachi>
2020-07-09 04:47:53 -0300

    Asunto : conversion de consulta - vista registro-testeo
    Descripcion : Agregue la vista nesesaria para seleccionar unidad y hacer
    todo el registro testeo.. habria que agregarle el boton Finalizar al
    formularioPersona, cree una nueva consulta, esta mapeada como
    "consulta", aun no elimine el consulta barrio por las dudas... configure
    el nav para que mapee desde cualquier sitio.
    
    Responsable : Aucachi Fabian

--- ProyectoFinal_Testeo_Covid19/src/main/java/ar/edu/unju/fi/controller/MainController.java
+++ ProyectoFinal_Testeo_Covid19/src/main/java/ar/edu/unju/fi/controller/MainController.java
@@ -1,12 +1,27 @@
 package ar.edu.unju.fi.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 //import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import ar.edu.unju.fi.service.IBarrioService;
+import ar.edu.unju.fi.service.IPersonaTesteadaService;
+import ar.edu.unju.fi.service.IUnidadHabitacionalService;
+
 @Controller
 public class MainController {
+	@Autowired
+	IBarrioService barrioService;
+	
+	@Autowired
+	IPersonaTesteadaService personaTesteadaService;
+	
+	
+	@Autowired
+	private IUnidadHabitacionalService iunidadService;
+	
 
 //@RequestMapping("inicio")
 	//public String getInicio(Model model) {
@@ -30,6 +45,19 @@ public class MainController {
 		return "usuarioBM";
 	}*/
 
+	@RequestMapping("/consulta")
+	public String getcons(Model model) {
+		model.addAttribute("listaPersonas", personaTesteadaService.listarPersonasTesteadas());
+		model.addAttribute("listaBarrios", barrioService.listarBarrios());
+		return "consulta";
+}
+	
+	
+	@RequestMapping("/seleccion")
+	public String getconsss(Model model) {
+		model.addAttribute("listaUnidad",iunidadService.listarUnidad());
+		return "seleccionarUnidad";
+}
 	
 	
 	
